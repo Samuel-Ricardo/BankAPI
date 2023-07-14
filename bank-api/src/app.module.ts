4c58aa5becc7e772e752904c973963039f71717b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 11:03:20 -0300

    [:card_file_box:] | setup: TypeORM (sqlite)

--- bank-api/src/app.module.ts
+++ bank-api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BankAccountSchema } from './@core/infra/db/bank-account.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: __dirname + '/db.sqlite',
+      synchronize: true,
+      logging: true,
+      entities: [BankAccountSchema],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

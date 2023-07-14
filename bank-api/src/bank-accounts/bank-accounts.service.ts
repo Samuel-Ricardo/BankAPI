8d453bad8fe12a06377fc8c3c17e6b5fb85d93c8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 11:57:42 -0300

    [:sparkles:] | create: service - bank-account > create (module)

--- bank-api/src/bank-accounts/bank-accounts.service.ts
+++ bank-api/src/bank-accounts/bank-accounts.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository, getDataSourceToken } from '@nestjs/typeorm';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { Repository, DataSource } from 'typeorm';
+import { BankAccountSchema } from '../@core/infra/db/bank-account.schema';
 
 @Injectable()
 export class BankAccountsService {
-  create(createBankAccountDto: CreateBankAccountDto) {
-    return 'This action adds a new bankAccount';
+  constructor(
+    @InjectRepository(BankAccountSchema)
+    private repository: Repository<BankAccountSchema>,
+    @Inject(getDataSourceToken())
+    private dataSource: DataSource,
+  ) {}
+
+  async create(createBankAccountDto: CreateBankAccountDto) {
+    const account = this.repository.create({
+      account_number: createBankAccountDto.account_number,
+      balance: 0,
+    });
+
+    await this.repository.insert(account);
+    return account;
   }
 
   findAll() {
@@ -16,7 +31,7 @@ export class BankAccountsService {
     return `This action returns a #${id} bankAccount`;
   }
 
-  update(id: number, updateBankAccountDto: UpdateBankAccountDto) {
+  update(id: number) {
     return `This action updates a #${id} bankAccount`;
   }
 

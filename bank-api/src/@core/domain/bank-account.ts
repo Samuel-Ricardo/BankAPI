c466925b148d3afc71f82435fbff6c052fbe6482
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 09:03:22 -0300

    [:sparkles:] | create: entity - BankAccount (@core)

---
+++ bank-api/src/@core/domain/bank-account.ts
@@ -0,0 +1,12 @@
+import { v4 as uuid } from 'typeorm/driver/mongodb/bson.typings';
+
+export class BankAccount {
+  constructor(
+    private balance: number,
+    private account_number: string,
+    private id?: string,
+  ) {
+    this.id = id ?? uuid();
+    ('');
+  }
+}

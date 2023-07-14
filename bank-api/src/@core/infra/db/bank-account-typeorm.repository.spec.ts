f52ab5703fdd32f7972816fd1109fdc351883217
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 10:49:47 -0300

    [:white_check_mark:] | setup: test - bank account > insert (repository)

--- bank-api/src/@core/infra/db/bank-account-typeorm.repository.spec.ts
+++ bank-api/src/@core/infra/db/bank-account-typeorm.repository.spec.ts
@@ -1,6 +1,7 @@
 import { DataSource, Repository } from 'typeorm';
 import { BankAccountSchema } from './bank-account.schema';
 import { BankAccountTypeOrmRepository } from './bank-account-typeorm.repository';
+import { BankAccount } from '../../domain/bank-account';
 
 describe('[REPOSITORY] | Bank Account - (TypeORM) ', () => {
   let dataSource: DataSource;
@@ -22,7 +23,16 @@ describe('[REPOSITORY] | Bank Account - (TypeORM) ', () => {
     repository = new BankAccountTypeOrmRepository(ormRepo);
   });
 
-  it('should create a bank account', async () => {
-    expect(true).toBeTruthy();
+  it('should insert a new bank account', async () => {
+    const bankAccount = new BankAccount(100, '111-11', '123');
+    await repository.insert(bankAccount);
+
+    const model = await ormRepo.findOneBy({ account_number: '111-11' });
+
+    expect(model).toBeDefined();
+
+    expect(model.id).toBe('123');
+    expect(model.balance).toBe(100);
+    expect(model.account_number).toBe('111-11');
   });
 });

8d1d786d371b6d73dd519833c77d8c797a294d90
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 11:27:30 -0300

    [:sparkles:] | update: module - bank-accounts (imports)

--- bank-api/src/bank-accounts/bank-accounts.module.ts
+++ bank-api/src/bank-accounts/bank-accounts.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { BankAccountsService } from './bank-accounts.service';
 import { BankAccountsController } from './bank-accounts.controller';
+import { TypeOrmModule, getDataSourceToken } from '@nestjs/typeorm';
+import { BankAccountSchema } from '../@core/infra/db/bank-account.schema';
+import { BankAccountTypeOrmRepository } from '../@core/infra/db/bank-account-typeorm.repository';
+import { DataSource } from 'typeorm';
+import { BankAccountRepository } from '..//@core/domain/bank-account.repository';
+import { BankAccountService } from '../@core/domain/bank-account.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BankAccountSchema])],
   controllers: [BankAccountsController],
-  providers: [BankAccountsService],
+  providers: [
+    BankAccountsService,
+    {
+      provide: BankAccountTypeOrmRepository,
+      useFactory: (dataSource: DataSource) => {
+        return new BankAccountTypeOrmRepository(
+          dataSource.getRepository(BankAccountSchema),
+        );
+      },
+      inject: [getDataSourceToken()],
+    },
+    {
+      provide: BankAccountService,
+      useFactory: (repo: BankAccountRepository) => {
+        return new BankAccountService(repo);
+      },
+      inject: [BankAccountTypeOrmRepository],
+    },
+  ],
 })
 export class BankAccountsModule {}

1ceb3c7360f5b9e3f3fb61143699285a0718c821
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 10:55:21 -0300

    [:white_check_mark:] | setup: test - bank account (service)

--- bank-api/src/@core/domain/bank-account.service.spec.ts
+++ bank-api/src/@core/domain/bank-account.service.spec.ts
@@ -1,5 +1,31 @@
+import { DataSource, Repository } from 'typeorm';
+import { BankAccountSchema } from '../infra/db/bank-account.schema';
+import { BankAccountTypeOrmRepository } from '../infra/db/bank-account-typeorm.repository';
+import { BankAccountService } from './bank-account.service';
+
 describe('[SERVICE] | Bank Account - Tests', () => {
-  it('should create a bank account', () => {
+  let dataSource: DataSource;
+  let ormRepo: Repository<BankAccountSchema>;
+  let service: BankAccountService;
+  let repository: BankAccountTypeOrmRepository;
+
+  beforeEach(async () => {
+    dataSource = new DataSource({
+      type: 'sqlite',
+      database: ':memory:',
+      synchronize: true,
+      logging: true,
+      entities: [BankAccountSchema],
+    });
+
+    await dataSource.initialize();
+
+    ormRepo = dataSource.getRepository(BankAccountSchema);
+    repository = new BankAccountTypeOrmRepository(ormRepo);
+    service = new BankAccountService(repository);
+  });
+
+  it('should create a new bank account', () => {
     expect(true).toBeTruthy();
   });
 });

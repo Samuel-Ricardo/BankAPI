bde06b40bca56dae1b86182c7d78529c505d63ac
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 09:50:10 -0300

    [:sparkles:] | create: service - bank-account > transfer (@core)

--- bank-api/src/@core/domain/bank-account.service.ts
+++ bank-api/src/@core/domain/bank-account.service.ts
@@ -1,5 +1,6 @@
 import { BankAccount } from './bank-account';
 import { BankAccountRepository } from './bank-account.repository';
+import { TransferService } from './transfer.service';
 
 export class BankAccountService {
   constructor(private bankAccountRepository: BankAccountRepository) {}
@@ -9,5 +10,23 @@ export class BankAccountService {
     await this.bankAccountRepository.insert(bankAccount);
 
     return bankAccount;
+  }
+
+  async transfer(
+    account_number_src: string,
+    account_number_dest: string,
+    amount: number,
+  ) {
+    const bankAccountSrc = await this.bankAccountRepository.findByAccountNumber(
+      account_number_src,
+    );
+    const bankAccountDest =
+      await this.bankAccountRepository.findByAccountNumber(account_number_dest);
+
+    const transferService = new TransferService();
+    await transferService.transfer(bankAccountSrc, bankAccountDest, amount);
+
+    await this.bankAccountRepository.update(bankAccountDest);
+    await this.bankAccountRepository.update(bankAccountSrc);
   }
 }

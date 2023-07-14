21476a583fa032dccbbe3bc2b18940c9d5af4047
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 10:34:32 -0300

    [:sparkles:] | implements: repository - bank-account > update (typeorm)

--- bank-api/src/@core/infra/db/bank-account-typeorm.repository.ts
+++ bank-api/src/@core/infra/db/bank-account-typeorm.repository.ts
@@ -7,13 +7,13 @@ export class BankAccountTypeOrmRepository implements BankAccountRepository {
   constructor(private ormRepo: Repository<BankAccountSchema>) {}
 
   async insert(bankAccount: BankAccount): Promise<void> {
-    const model = this.ormRepo.create(
-      bankAccount as unknown as BankAccountSchema,
-    );
+    const model = this.ormRepo.create({ ...bankAccount });
     await this.ormRepo.insert(model);
   }
-  update(bankAccount: BankAccount): Promise<void> {
-    throw new Error('Method not implemented.');
+  async update(bankAccount: BankAccount): Promise<void> {
+    await this.ormRepo.update(bankAccount.getId(), {
+      balance: bankAccount.getBalance(),
+    });
   }
   findByAccountNumber(accountNumber: string): Promise<BankAccount> {
     throw new Error('Method not implemented.');

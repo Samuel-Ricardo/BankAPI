bd6fde51c0a20b0aae85c6b44b431ec4b63a759b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-14 11:58:31 -0300

    [:white_check_mark:] | setup: test - bank account (service)

--- bank-api/src/bank-accounts/bank-accounts.controller.spec.ts
+++ bank-api/src/bank-accounts/bank-accounts.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { getDataSourceToken, getRepositoryToken } from '@nestjs/typeorm';
 import { BankAccountsController } from './bank-accounts.controller';
 import { BankAccountsService } from './bank-accounts.service';
+import { BankAccountTypeOrmRepository } from '../@core/infra/db/bank-account-typeorm.repository';
+import { BankAccountService } from '../@core/domain/bank-account.service';
+import { BankAccountRepository } from '../@core/domain/bank-account.repository';
+import { BankAccountSchema } from '../@core/infra/db/bank-account.schema';
 
 describe('BankAccountsController', () => {
   let controller: BankAccountsController;
@@ -8,7 +14,47 @@ describe('BankAccountsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BankAccountsController],
-      providers: [BankAccountsService],
+      providers: [
+        BankAccountsService,
+        {
+          provide: getRepositoryToken(BankAccountSchema),
+          useClass: BankAccountTypeOrmRepository,
+        },
+        {
+          provide: DataSource,
+          useFactory: async () => {
+            const dataSource = new DataSource({
+              type: 'sqlite',
+              database: ':memory:',
+              synchronize: true,
+              logging: true,
+              entities: [BankAccountSchema],
+            });
+
+            if (!dataSource.isInitialized) {
+              await dataSource.initialize();
+            }
+
+            return dataSource;
+          },
+        },
+        {
+          provide: BankAccountTypeOrmRepository,
+          useFactory: (dataSource: DataSource) => {
+            return new BankAccountTypeOrmRepository(
+              dataSource.getRepository(BankAccountSchema),
+            );
+          },
+          inject: [getDataSourceToken()],
+        },
+        {
+          provide: BankAccountService,
+          useFactory: (repo: BankAccountRepository) => {
+            return new BankAccountService(repo);
+          },
+          inject: [BankAccountTypeOrmRepository],
+        },
+      ],
     }).compile();
 
     controller = module.get<BankAccountsController>(BankAccountsController);
--- bank-api/src/bank-accounts/bank-accounts.service.spec.ts
+++ bank-api/src/bank-accounts/bank-accounts.service.spec.ts
@@ -1,12 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getDataSourceToken, getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { BankAccountsService } from './bank-accounts.service';
+import { BankAccountTypeOrmRepository } from '../@core/infra/db/bank-account-typeorm.repository';
+import { BankAccountService } from '../@core/domain/bank-account.service';
+import { BankAccountRepository } from '../@core/domain/bank-account.repository';
+import { BankAccountSchema } from '../@core/infra/db/bank-account.schema';
 
 describe('BankAccountsService', () => {
   let service: BankAccountsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BankAccountsService],
+      providers: [
+        BankAccountsService,
+        {
+          provide: getRepositoryToken(BankAccountSchema),
+          useClass: BankAccountTypeOrmRepository,
+        },
+        {
+          provide: DataSource,
+          useFactory: async () => {
+            const dataSource = new DataSource({
+              type: 'sqlite',
+              database: ':memory:',
+              synchronize: true,
+              logging: true,
+              entities: [BankAccountSchema],
+            });
+
+            if (!dataSource.isInitialized) {
+              await dataSource.initialize();
+            }
+
+            return dataSource;
+          },
+        },
+        {
+          provide: BankAccountTypeOrmRepository,
+          useFactory: (dataSource: DataSource) => {
+            return new BankAccountTypeOrmRepository(
+              dataSource.getRepository(BankAccountSchema),
+            );
+          },
+          inject: [getDataSourceToken()],
+        },
+        {
+          provide: BankAccountService,
+          useFactory: (repo: BankAccountRepository) => {
+            return new BankAccountService(repo);
+          },
+          inject: [BankAccountTypeOrmRepository],
+        },
+      ],
     }).compile();
 
     service = module.get<BankAccountsService>(BankAccountsService);
